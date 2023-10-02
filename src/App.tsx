7c86363adce63f6b965e3fd63c346711509a74f2
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-10-02 09:35:03 +0545

    consolidating state logic with a Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css';
+import "./App.css";
+import Counter from "./state-management/Counter";
+import HomePage from "./state-management/HomePage";
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return <Counter />;
 }
 
 export default App;
