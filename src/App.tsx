2b27bbbbde9045bc456af6c529244aae774a6d6c
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-10-03 10:13:09 +0545

    sharing state using the react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useReducer } from "react";
 import "./App.css";
 import Counter from "./state-management/Counter";
 import HomePage from "./state-management/HomePage";
 import LoginStatus from "./state-management/LoginStatus";
 import TaskList from "./state-management/TaskList";
+import tasksReducer from "./state-management/reducers/tasksReducer";
+import NavBar from "./state-management/NavBar";
+import TasksContext from "./state-management/contexts/tasksContext";
 
 function App() {
-  return <LoginStatus />;
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  // to share state using react context
+  // 1. lift the state to closest parent
+  // 2. create a context contexts/tasksContext.ts
+  //    a. context is like truck for transporting data
+  //    b. we should specify data as for transport
+  // 3. wrap that context to component tree using context Provider component and data you want to share
+  // 4. accest the share state in component tree component using the hook useContext(Context Name)
+  return (
+    <>
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+    </>
+  );
 }
 
 export default App;
--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,9 +1,12 @@
-import LoginStatus from './LoginStatus';
+import { useContext } from "react";
+import LoginStatus from "./LoginStatus";
+import TasksContext from "./contexts/tasksContext";
 
 const NavBar = () => {
+  const { tasks } = useContext(TasksContext); // we don't know dispatch, so we are just grabbing tasks
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">4</span>
+      <span className="badge text-bg-secondary">{tasks.length}</span>
       <LoginStatus />
     </nav>
   );
